37b01e82fb6b71f1b22a8a3e2e10a749af527bed
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-10-13 22:28:05 +0530

    refactor

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
@@ -7,14 +7,37 @@ import IconButton from "@mui/material/IconButton"
 import Tabs from "@mui/material/Tabs"
 import Tab from "@mui/material/Tab"
 import { styled } from "@mui/material/styles"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import SwipeableDrawer from "@mui/material/SwipeableDrawer"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemText from "@mui/material/ListItemText"
+import ListItemButton from "@mui/material/ListItemButton"
 import search from "../../images/search.svg"
 import cart from "../../images/cart.svg"
 import account from "../../images/account-header.svg"
+import menu from "../../images/menu.svg"
 
 const Header = ({ categories }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const matchesMD = useMediaQuery(theme => theme.breakpoints.down("md"))
+  const iOS =
+    typeof navigator !== "undefined" &&
+    /iPad|iPhone|iPod/.test(navigator.userAgent)
   const routes = [
     ...categories.map(({ node }) => node),
     { name: "Contact Us", strapi_id: "contact" },
+  ]
+  const actions = [
+    { icon: search, alt: "search", visible: true },
+    { icon: cart, alt: "cart", visible: true },
+    { icon: account, alt: "account", visible: !matchesMD },
+    {
+      icon: menu,
+      alt: "menu",
+      visible: matchesMD,
+      onClick: () => setDrawerOpen(true),
+    },
   ]
 
   // sx prop
@@ -28,6 +51,9 @@ const Header = ({ categories }) => {
         color: "",
       },
     },
+    accountButton: {
+      display: { xs: "none", md: "block" },
+    },
   }
 
   // styled components
@@ -35,29 +61,53 @@ const Header = ({ categories }) => {
     color: theme.palette.common.offBlack,
   }))
 
+  const Icon = styled("img")(() => ({
+    height: "3rem",
+    width: "3rem",
+  }))
+
   return (
     <header>
       <AppBar color="transparent" elevation={0}>
         <Toolbar>
-          <Button>
+          <Button disableRipple>
             <Typography variant="h1">
               <LogoText>VAR</LogoText> X
             </Typography>
           </Button>
-          <Tabs value={0} sx={sx.tabs}>
-            {routes.map(route => (
-              <Tab key={route.strapi_id} label={route.name} />
+          {matchesMD ? (
+            <SwipeableDrawer
+              anchor="right"
+              open={drawerOpen}
+              onOpen={() => setDrawerOpen(true)}
+              onClose={() => setDrawerOpen(false)}
+              disableBackdropTransition={!iOS}
+              disableDiscovery={iOS}
+            >
+              <List disablePadding>
+                {routes.map(route => (
+                  <ListItem divider key={route.strapi_id}>
+                    <ListItemButton>
+                      <ListItemText primary={route.name} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </SwipeableDrawer>
+          ) : (
+            <Tabs value={0} sx={sx.tabs}>
+              {routes.map(route => (
+                <Tab key={route.strapi_id} label={route.name} />
+              ))}
+            </Tabs>
+          )}
+          {actions
+            .filter(action => action.visible)
+            .map((action, i) => (
+              <IconButton key={i} onClick={action.onClick}>
+                <Icon src={action.icon} alt={action.alt} />
+              </IconButton>
             ))}
-          </Tabs>
-          <IconButton>
-            <img src={search} alt="search" />
-          </IconButton>
-          <IconButton>
-            <img src={cart} alt="cart" />
-          </IconButton>
-          <IconButton>
-            <img src={account} alt="account" />
-          </IconButton>
         </Toolbar>
       </AppBar>
     </header>
