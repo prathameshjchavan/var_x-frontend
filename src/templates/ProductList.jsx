a670a246c2be10da1f3b4d1091c7f688761ab2a7
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-11-04 20:13:32 +0530

    category products page query

--- src/templates/ProductList.jsx
+++ src/templates/ProductList.jsx
@@ -1,9 +1,13 @@
 import { Grid } from "@mui/material"
+import { graphql } from "gatsby"
 import React from "react"
 import DynamicToolbar from "../components/product-list/DynamicToolbar"
 import Layout from "../components/ui/layout"
 
-const ProductList = ({ pageContext: { filterOptions, name, description } }) => {
+const ProductList = ({
+  pageContext: { filterOptions, name, description },
+  data,
+}) => {
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
@@ -16,5 +20,28 @@ const ProductList = ({ pageContext: { filterOptions, name, description } }) => {
     </Layout>
   )
 }
+
+export const query = graphql`
+  query GetCategoryProducts($id: Int!) {
+    allStrapiProduct(filter: { category: { strapi_id: { eq: $id } } }) {
+      edges {
+        node {
+          strapi_id
+          name
+          variants {
+            color
+            id
+            size
+            price
+            style
+            images {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default ProductList
