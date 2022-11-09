7449ed67593ac2b613721e26ae0d264ab09f1ed1
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-11-09 12:35:03 +0530

    add to cart rebuild

--- src/components/product-list/QtyButton.js
+++ src/components/product-list/QtyButton.js
@@ -1,5 +1,5 @@
-import { Button, ButtonGroup, Grid, Typography, useTheme } from "@mui/material"
-import cart from "../../images/cart.svg"
+import { Badge, Button, Grid, Typography, useTheme } from "@mui/material"
+import Cart from "../../images/Cart"
 import React, { useState } from "react"
 
 const QtyButton = () => {
@@ -8,68 +8,118 @@ const QtyButton = () => {
 
   // sx prop
   const sx = {
-    buttonGroup: {
+    mainContainer: {
+      marginTop: "2.25rem",
       height: "3rem",
+    },
+    buttonContainer: {
+      width: "fit-content",
       border: `3px solid ${theme.palette.secondary.light}`,
-      display: "flex",
-      alignItems: "center",
-      maxWidth: "fit-content",
+      borderRadius: 50,
     },
-    editButtonGroup: {
-      borderWidth: "0px 3px",
+    button: {
+      background: theme.palette.secondary.main,
+      border: "none !important",
       borderRadius: 0,
-      borderColor: theme.palette.secondary.light,
+    },
+    editButtonContainer: {
+      "& :first-of-type .MuiButton-root": {
+        height: "25.5px",
+        borderBottom: "3px solid #fff !important",
+      },
+      "& :last-of-type .MuiButton-root > .MuiTypography-root": {
+        marginTop: "-0.3rem",
+      },
+      borderWidth: "0 3px 0 3px",
       borderStyle: "solid",
-      height: "105%",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
+      borderColor: "#fff",
+      width: "3.5rem",
     },
-    editButtons: {
-      height: "1.5rem",
-      width: "105%",
-      "&:first-of-type": {
-        borderBottom: `1px solid ${theme.palette.secondary.light} !important`,
-      },
-      "&:last-of-type": {
-        borderTop: `1px solid ${theme.palette.secondary.light} !important`,
-      },
-      borderRadius: 0,
+    editButton: {
+      height: "22.5px",
+      padding: 0,
+      minWidth: 0,
+      width: "100%",
     },
-    button: { border: "none !important" },
+    qtyButton: {
+      borderRadius: "50px 0 0 50px",
+      width: "3rem",
+      minWidth: 0,
+      height: "3rem",
+      "& .MuiTypography-root": {
+        marginRight: "-0.3rem",
+      },
+      "&:hover": { backgroundColor: theme.palette.secondary.main },
+    },
+    cartButton: {
+      borderRadius: "0 50px 50px 0",
+      overflow: "hidden",
+      height: "3rem",
+      "&:hover .MuiBadge-badge": {
+        backgroundColor: theme.palette.primary.main,
+      },
+      transition: "none",
+    },
     qtyText: {
       color: "#fff",
+    },
+    badge: {
+      "& .MuiBadge-badge": {
+        fontSize: "1.5rem",
+        padding: 0,
+      },
     },
   }
 
   return (
-    <Grid item>
-      <ButtonGroup variant="outlined" sx={sx.buttonGroup}>
-        <Button disableRipple sx={sx.button}>
-          <Typography variant="h3" sx={sx.qtyText}>
-            {qty}
-          </Typography>
-        </Button>
-        <ButtonGroup sx={sx.editButtonGroup}>
-          <Button disableRipple sx={{ ...sx.button, ...sx.editButtons }}>
+    <Grid item sx={sx.mainContainer}>
+      <Grid container sx={sx.buttonContainer}>
+        <Grid item>
+          <Button disableRipple sx={{ ...sx.button, ...sx.qtyButton }}>
             <Typography variant="h3" sx={sx.qtyText}>
-              +
+              {qty}
             </Typography>
           </Button>
-          <Button disableRipple sx={{ ...sx.button, ...sx.editButtons }}>
-            <Typography
-              variant="h3"
-              sx={{ ...sx.qtyText, marginTop: "-0.3rem" }}
+        </Grid>
+        <Grid item>
+          <Grid container direction="column" sx={sx.editButtonContainer}>
+            <Grid item>
+              <Button
+                disableRipple
+                onClick={() => setQty(qty + 1)}
+                sx={{ ...sx.button, ...sx.editButton }}
+              >
+                <Typography variant="h3" sx={sx.qtyText}>
+                  +
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                disableRipple
+                onClick={() => setQty(qty - 1)}
+                sx={{ ...sx.button, ...sx.editButton }}
+              >
+                <Typography variant="h3" sx={sx.qtyText}>
+                  -
+                </Typography>
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button disableRipple sx={{ ...sx.button, ...sx.cartButton }}>
+            <Badge
+              color="secondary"
+              sx={sx.badge}
+              overlap="circular"
+              badgeContent="+"
             >
-              -
-            </Typography>
+              <Cart color="#fff" />
+            </Badge>
           </Button>
-        </ButtonGroup>
-        <Button disableRipple sx={sx.button}>
-          <img src={cart} alt="add to cart" />
-        </Button>
-      </ButtonGroup>
+        </Grid>
+      </Grid>
     </Grid>
   )
 }
