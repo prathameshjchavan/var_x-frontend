63f83177ac06ff2fccb919c36d59e6181f06294d
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-11-09 16:55:20 +0530

    go to top button

--- src/templates/ProductList.jsx
+++ src/templates/ProductList.jsx
@@ -1,6 +1,7 @@
-import { Grid } from "@mui/material"
+import { Fab, Grid } from "@mui/material"
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward"
 import { graphql } from "gatsby"
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import DynamicToolbar from "../components/product-list/DynamicToolbar"
 import ListOfProducts from "../components/product-list/ListOfProducts"
 import Layout from "../components/ui/layout"
@@ -12,10 +13,33 @@ const ProductList = ({
   },
 }) => {
   const [layout, setLayout] = useState("grid")
+  const scrollRef = useRef(null)
+
+  // sx prop
+  const sx = {
+    fab: {
+      alignSelf: "flex-end",
+      marginRight: "2rem",
+      marginBottom: "2rem",
+      color: "#fff",
+      width: "5rem",
+      height: "5rem",
+    },
+    fabIcon: {
+      width: "5rem",
+      height: "5rem",
+    },
+  }
+
+  const scroll = () => {
+    scrollRef.current.scrollIntoView({ behavior: "smooth" })
+  }
 
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
+        {/* Ref for Dynamic Toolbar */}
+        <div ref={scrollRef} />
         <DynamicToolbar
           filterOptions={filterOptions}
           name={name}
@@ -24,6 +48,9 @@ const ProductList = ({
           setLayout={setLayout}
         />
         <ListOfProducts products={products} layout={layout} />
+        <Fab sx={sx.fab} color="primary" onClick={scroll}>
+          <ArrowUpwardIcon sx={sx.fabIcon} />
+        </Fab>
       </Grid>
     </Layout>
   )
