6174b448028669ea42059285829d9b375cd9a594
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-11-23 20:03:53 +0530

    product detail images

---
+++ src/components/product-details/ProductImages.js
@@ -0,0 +1,59 @@
+import { Grid, IconButton } from "@mui/material"
+import { styled } from "@mui/material/styles"
+import React from "react"
+
+const ProductImages = ({ images, selectedImage, setSelectedImage }) => {
+  // sx prop
+  const sx = {
+    productImages: {
+      width: "50%",
+    },
+    selected: {
+      height: "40rem",
+      width: "40rem",
+    },
+    small: {
+      height: "5rem",
+      width: "5rem",
+    },
+    imageItem: {
+      margin: "1rem",
+    },
+  }
+
+  // styled components
+  const ProductImage = styled("img")(() => ({}))
+
+  return (
+    <Grid
+      item
+      container
+      sx={sx.productImages}
+      direction="column"
+      alignItems="center"
+    >
+      <Grid item>
+        <ProductImage
+          src={process.env.STRAPI_API_URL + images[selectedImage].url}
+          sx={sx.selected}
+          alt="product_large"
+        />
+      </Grid>
+      <Grid item container justifyContent="center">
+        {images.map(({ url }, index) => (
+          <Grid key={url} sx={sx.imageItem} item>
+            <IconButton onClick={() => setSelectedImage(index)}>
+              <ProductImage
+                src={process.env.STRAPI_API_URL + url}
+                sx={sx.small}
+                alt={`product_small_${index}`}
+              />
+            </IconButton>
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default ProductImages
--- src/templates/ProductDetail.jsx
+++ src/templates/ProductDetail.jsx
@@ -1,7 +1,26 @@
-import React from "react"
+import { Grid } from "@mui/material"
+import React, { useState } from "react"
+import ProductImages from "../components/product-details/ProductImages"
+import Layout from "../components/ui/layout"
 
-const ProductDetail = ({ pageContext }) => {
-  return <div>{pageContext.name}</div>
+const ProductDetail = ({
+  pageContext: { id, name, category, description, variants },
+}) => {
+  const [selectedVariant, setSelectedVariant] = useState(0)
+  const [selectedImage, setSelectedImage] = useState(0)
+
+  return (
+    <Layout>
+      <Grid container direction="column">
+        <ProductImages
+          images={variants[selectedVariant].images}
+          selectedImage={selectedImage}
+          setSelectedImage={setSelectedImage}
+        />
+        <Grid />
+      </Grid>
+    </Layout>
+  )
 }
 
 export default ProductDetail
