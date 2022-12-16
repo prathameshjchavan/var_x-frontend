b089cb8898eccd8551317ee429daaa0678fae836
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-12-16 17:08:19 +0530

    logout user

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,11 +1,29 @@
-import React from "react"
+import { Button } from "@mui/material"
+import React, { useCallback, useContext } from "react"
 import AuthPortal from "../components/auth/AuthPortal"
 import Layout from "../components/ui/layout"
+import { UserContext } from "../contexts"
+import { setUser } from "../contexts/actions"
 
-const Account = () => (
-  <Layout>
-    <AuthPortal />
-  </Layout>
-)
+const Account = () => {
+  const { user, dispatchUser, defaultUser } = useContext(UserContext)
+
+  // functions
+  const handleLogout = useCallback(() => {
+    dispatchUser(setUser(defaultUser))
+  }, [dispatchUser, defaultUser])
+
+  return (
+    <Layout>
+      {user.jwt ? (
+        <Button variant="contained" onClick={handleLogout}>
+          Logout
+        </Button>
+      ) : (
+        <AuthPortal />
+      )}
+    </Layout>
+  )
+}
 
 export default Account
