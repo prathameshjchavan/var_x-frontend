69ba6b712012e8f4fc6081b8a10489a5f861ee7a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-12-17 16:02:00 +0530

    reset user password - 2

--- src/components/auth/reset.js
+++ src/components/auth/reset.js
@@ -3,7 +3,9 @@ import { getEmailPasswordFields } from "./Login"
 import accountIcon from "../../images/account.svg"
 import { Button, Grid, Typography } from "@mui/material"
 import Fields from "./Fields"
+import CircularProgress from "@mui/material/CircularProgress"
 import { setSnackbar } from "../../contexts/actions"
+import axios from "axios"
 
 const Reset = ({ steps, setSelectedStep, dispatchFeedback }) => {
   const [visible, setVisible] = useState(false)
@@ -12,7 +14,14 @@ const Reset = ({ steps, setSelectedStep, dispatchFeedback }) => {
     confirmation: "",
   })
   const [errors, setErrors] = useState({})
-
+  const [loading, setLoading] = useState(false)
+  const disabled = useMemo(
+    () =>
+      Object.keys(errors).some(error => errors[error] === true) ||
+      Object.keys(errors).length !== Object.keys(values).length ||
+      values.password !== values.confirmation,
+    [errors, values]
+  )
   const fields = useMemo(() => {
     const { password } = getEmailPasswordFields(
       true,
@@ -44,6 +53,8 @@ const Reset = ({ steps, setSelectedStep, dispatchFeedback }) => {
 
   // functions
   const handleReset = useCallback(() => {
+    setLoading(true)
+
     const params = new URLSearchParams(window.location.search)
     const code = params.get("code")
 
@@ -53,9 +64,30 @@ const Reset = ({ steps, setSelectedStep, dispatchFeedback }) => {
         password: values.password,
         passwordConfirmation: values.confirmation,
       })
-      .then(response => {})
-      .catch(error => {})
-  }, [])
+      .then(response => {
+        dispatchFeedback(
+          setSnackbar({
+            status: "success",
+            message: "Password Reset Successfully",
+          })
+        )
+
+        setTimeout(() => {
+          window.history.replaceState(null, null, window.location.pathname)
+
+          const loginStep = steps.findIndex(step => step.label === "Login")
+          setSelectedStep(loginStep)
+        }, 6000)
+      })
+      .catch(error => {
+        const { message } = error.response.data.error
+        console.error(error)
+        dispatchFeedback(setSnackbar({ status: "error", message }))
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [dispatchFeedback, setSelectedStep, steps, values])
 
   return (
     <Fragment>
@@ -72,11 +104,16 @@ const Reset = ({ steps, setSelectedStep, dispatchFeedback }) => {
       <Grid item>
         <Button
           variant="contained"
+          disabled={disabled}
           onClick={handleReset}
           color="secondary"
           sx={sx.reset}
         >
-          <Typography variant="h5">Reset Password</Typography>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <Typography variant="h5">Reset Password</Typography>
+          )}
         </Button>
       </Grid>
     </Fragment>
