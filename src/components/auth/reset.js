c7a1e62c4f70bfe73bf2f37a5b88417ee1142ee5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-12-17 12:04:42 +0530

    reset password page

--- src/components/auth/reset.js
+++ src/components/auth/reset.js
@@ -1,15 +1,44 @@
-import React, { useMemo, useState } from "react"
+import React, { Fragment, useMemo, useState } from "react"
 import { getEmailPasswordFields } from "./Login"
+import accountIcon from "../../images/account.svg"
+import { Grid } from "@mui/material"
+import Fields from "./Fields"
 
 const Reset = () => {
   const [visible, setVisible] = useState(false)
+  const [values, setValues] = useState({
+    password: "",
+    confirmation: "",
+  })
+  const [errors, setErrors] = useState({})
 
-  const fields = useMemo(
-    () => getEmailPasswordFields(true, false, visible, setVisible),
-    [visible, setVisible]
+  const fields = useMemo(() => {
+    const { password } = getEmailPasswordFields(
+      true,
+      false,
+      visible,
+      setVisible
+    )
+    return {
+      password,
+      confirmation: { ...password, placeholder: "Confirm Password" },
+    }
+  }, [visible, setVisible])
+
+  return (
+    <Fragment>
+      <Grid item>
+        <img src={accountIcon} alt="reset password page" />
+      </Grid>
+      <Fields
+        fields={fields}
+        values={values}
+        setValues={setValues}
+        errors={errors}
+        setErrors={setErrors}
+      />
+    </Fragment>
   )
-
-  return <div>reset</div>
 }
 
 export default Reset
