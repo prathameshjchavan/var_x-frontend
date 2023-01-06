7f32bc6e8749dd9c41ba6e6ee97de28ae782bb4a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-06 11:53:32 +0530

    cart context reducer - 2

--- src/contexts/reducers/cart-reducer.js
+++ src/contexts/reducers/cart-reducer.js
@@ -5,14 +5,12 @@ import {
 } from "../actions/action-types"
 
 export default function cartReducer(state, action) {
+  const { variant, qty, stock } = action.payload
   let newCart = [...state]
-
   let existingIndex
 
   if (action.payload) {
-    existingIndex = state.findIndex(
-      item => item.variant === action.payload.variant
-    )
+    existingIndex = state.findIndex(item => item.variant === variant)
   }
 
   const saveData = cart => {
@@ -21,8 +19,37 @@ export default function cartReducer(state, action) {
 
   switch (action.type) {
     case "ADD_TO_CART":
+      if (existingIndex !== -1) {
+        let newQty = newCart[existingIndex].qty + qty
+
+        if (newQty > stock) {
+          newQty = stock
+        }
+
+        newCart[existingIndex] = { ...newCart[existingIndex], qty: newQty }
+      } else {
+        newCart.push(action.payload)
+      }
+
+      saveData(newCart)
+
+      return newCart
     case "REMOVE_FROM_CART":
+      const newQty = newCart[existingIndex].qty - qty
+
+      if (newQty <= 0) {
+        newCart = newCart.filter(item => item.variant !== variant)
+      } else {
+        newCart[existingIndex] = { ...newCart[existingIndex], qty: newQty }
+      }
+
+      saveData(newCart)
+
+      return newCart
     case "CLEAR_CART":
+      localStorage.removeItem("cart")
+
+      return []
     default:
       return state
   }
