a4303d9a8f89c3c47e13c5d85916fe190f88e268
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-07 10:52:01 +0530

    display cart items - 2

--- src/components/cart/Item.js
+++ src/components/cart/Item.js
@@ -1,14 +1,30 @@
-import { Grid, Typography, useTheme } from "@mui/material"
+import React, { useMemo } from "react"
+import { Grid, Typography, Chip, IconButton, useTheme } from "@mui/material"
 import { styled } from "@mui/material/styles"
-import React from "react"
+import QtyButton from "../product-list/QtyButton"
+import favoriteIcon from "../../images/favorite.svg"
+import subscriptionIcon from "../../images/subscription.svg"
+import deleteIcon from "../../images/delete.svg"
 
 const Item = ({ item }) => {
   const theme = useTheme()
+  const actions = useMemo(
+    () => [
+      { icon: favoriteIcon },
+      { icon: subscriptionIcon },
+      { icon: deleteIcon },
+    ],
+    []
+  )
 
   // sx prop
   const sx = {
     name: {
       color: theme.palette.secondary.main,
+    },
+    id: {
+      color: theme.palette.secondary.main,
+      fontSize: "1rem",
     },
   }
 
@@ -32,6 +48,32 @@ const Item = ({ item }) => {
             <Typography variant="h5" sx={sx.name}>
               {item.name}
             </Typography>
+          </Grid>
+          <Grid item>
+            <QtyButton
+              variant={item.variant}
+              name={item.name}
+              stock={{ attributes: { quantity: item.stock } }}
+            />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Chip label={`$${item.variant.price}`} color="secondary" />
+        </Grid>
+        <Grid item container justifyContent="space-between">
+          <Grid item>
+            <Typography variant="body1" sx={sx.id}>
+              ID: {item.variant.id}
+            </Typography>
+          </Grid>
+          <Grid item container>
+            {actions.map((action, index) => (
+              <Grid item key={index}>
+                <IconButton>
+                  <img src={action.icon} />
+                </IconButton>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
       </Grid>
