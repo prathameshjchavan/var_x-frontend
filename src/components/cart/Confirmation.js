591314010b17babe316ef0f3d1d2754ff70cfb83
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-09 12:04:27 +0530

    Confirmation Fields

--- src/components/cart/Confirmation.js
+++ src/components/cart/Confirmation.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useMemo } from "react"
 import ConfirmationIcon from "../../images/tag.svg"
 import NameAdornment from "../../images/NameAdornment"
 import EmailAdornment from "../../images/EmailAdornment"
@@ -6,41 +6,109 @@ import PhoneAdornment from "../../images/PhoneAdornment"
 import StreetAdornment from "../../images/street-adornment.svg"
 import zipAdornment from "../../images/zip-adornment.svg"
 import cardAdornment from "../../images/card.svg"
-import promoAdornment from "../../images/promo-adornment.svg"
+import promoAdornment from "../../images/promo-code.svg"
 import { Grid, Typography } from "@mui/material"
 import { styled } from "@mui/material/styles"
 
 const Confirmation = () => {
   // sx prop
-  const sx = {
-    name: {
-      fontSize: "1rem",
-      color: "#fff",
-    },
-  }
+  const sx = useMemo(
+    () => ({
+      text: {
+        fontSize: "1rem",
+        color: "#fff",
+      },
+      nameWrapper: {
+        height: "22px",
+        width: "22px",
+      },
+      emailWrapper: {
+        height: "17px",
+        width: "22px",
+      },
+      phoneWrapper: {
+        height: "25.122px",
+        width: "25.173px",
+      },
+    }),
+    []
+  )
 
   // styled components
-  const NameWrapper = styled("div")(() => ({
-    height: "22px",
-    width: "22px",
-  }))
+  const Wrapper = styled("div")(() => {})
+
+  // fields
+  const firstFields = useMemo(
+    () => [
+      {
+        value: "Prathamesh Chavan",
+        adornment: (
+          <Wrapper sx={sx.nameWrapper}>
+            <NameAdornment color="#fff" />
+          </Wrapper>
+        ),
+      },
+      {
+        value: "prathamesh.chavan216@gmail.com",
+        adornment: (
+          <Wrapper sx={sx.emailWrapper}>
+            <EmailAdornment color="#fff" />
+          </Wrapper>
+        ),
+      },
+      {
+        value: "(555) 555-5555",
+        adornment: (
+          <Wrapper sx={sx.phoneWrapper}>
+            <PhoneAdornment />
+          </Wrapper>
+        ),
+      },
+      {
+        value: "1234 Example Street",
+        adornment: <img src={StreetAdornment} alt="street address" />,
+      },
+    ],
+    [sx]
+  )
+
+  const secondFields = useMemo(
+    () => [
+      {
+        value: "Wichita, KS 67211",
+        adornment: <img src={zipAdornment} alt="city, state, zip code" />,
+      },
+      {
+        value: "**** **** **** 1234",
+        adornment: <img src={cardAdornment} alt="credit card" />,
+      },
+      {
+        promo: {
+          helperText: "",
+          placeholder: "Promo Code",
+          startAdornment: <img src={promoAdornment} alt="promo code" />,
+        },
+      },
+    ],
+    []
+  )
 
   return (
     <Grid item container direction="column">
       <Grid item container>
         <Grid item container direction="column" xs={8}>
-          <Grid item container>
-            <Grid item xs={1}>
-              <NameWrapper>
-                <NameAdornment color="#fff" />
-              </NameWrapper>
+          {firstFields.map((field, index) => (
+            <Grid item container key={index}>
+              <Grid item xs={1}>
+                {field.adornment}
+              </Grid>
+              <Grid item xs={11}>
+                <Typography variant="body1" sx={sx.text}>
+                  {field.value}
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={11}>
-              <Typography variant="body1" sx={sx.name}>
-                Prathamesh
-              </Typography>
-            </Grid>
-          </Grid>
+          ))}
         </Grid>
         <Grid item xs={4}>
           <img src={ConfirmationIcon} alt="confirmation" />
