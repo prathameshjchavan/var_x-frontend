f4fb423c2675284d2941cec4133293c40dc4a6a2
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-01-26 12:15:00 +0530

    data grid columns

--- src/components/settings/OrderHistory.js
+++ src/components/settings/OrderHistory.js
@@ -1,12 +1,30 @@
 import React, { useContext, useEffect, useState } from "react"
 import axios from "axios"
 import { UserContext } from "../../contexts"
+import { DataGrid } from "@mui/x-data-grid"
+import { Grid } from "@mui/material"
 
 const OrderHistory = () => {
-  const [order, setOrder] = useState([])
+  const [orders, setOrders] = useState([])
   const { user } = useContext(UserContext)
+
+  // DataGrid data
+  const columns = [
+    { field: "shipping", headerName: "Shipping", flex: 1 },
+    { field: "order", headerName: "Order", flex: 1 },
+    { field: "status", headerName: "Status", flex: 1 },
+    { field: "date", headerName: "Date", flex: 1 },
+    { field: "total", headerName: "Total", flex: 1 },
+  ]
+  const rows = []
+
   // sx prop
-  const sx = {}
+  const sx = {
+    item: {
+      height: "100%",
+      width: "100%",
+    },
+  }
 
   // useEffect
   useEffect(() => {
@@ -22,7 +40,11 @@ const OrderHistory = () => {
       })
   }, [])
 
-  return <div>OrderHistory</div>
+  return (
+    <Grid item sx={sx.item}>
+      <DataGrid columns={columns} rows={rows} pageSize={5} />
+    </Grid>
+  )
 }
 
 export default OrderHistory
