34d9247f23e344995690b6d5bca82e7dc8ff3d37
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-02-02 20:28:23 +0530

    paginate reviews

--- src/components/product-details/ProductReviews.js
+++ src/components/product-details/ProductReviews.js
@@ -1,19 +1,41 @@
-import { Grid } from "@mui/material"
-import React, { useState, useEffect, useContext } from "react"
+import { Grid, Pagination, useTheme } from "@mui/material"
+import React, { useState, useEffect, useContext, useMemo } from "react"
 import ProductReview from "./ProductReview"
 import { useQuery } from "@apollo/client"
 import { GET_REVIEWS } from "../../apollo/queries"
 import { UserContext } from "../../contexts"
 
 const ProductReviews = ({ product, edit, setEdit }) => {
+  const theme = useTheme()
   const [reviews, setReviews] = useState([])
+  const [page, setPage] = useState(1)
   const { user } = useContext(UserContext)
   const { data } = useQuery(GET_REVIEWS, { variables: { id: product } })
+  const reviewsperPage = 15
+  const numPages = useMemo(
+    () => Math.ceil(reviews.length / reviewsperPage),
+    [reviews, reviewsperPage]
+  )
 
   // sx prop
   const sx = {
     reviews: {
       padding: "0 3rem",
+    },
+    pagination: {
+      marginBottom: "3rem",
+      "& .MuiPaginationItem-text": {
+        "&:not(.Mui-disabled)": {
+          fontFamily: "Montserrat",
+          fontSize: "2rem",
+        },
+        "&:not(.Mui-selected)": {
+          color: theme.palette.primary.main,
+        },
+      },
+      "& .Mui-selected": {
+        color: "#fff !important",
+      },
     },
   }
 
@@ -48,6 +70,7 @@ const ProductReviews = ({ product, edit, setEdit }) => {
       )}
       {reviews
         .filter(review => (edit ? review.user.name !== user.name : true))
+        .slice((page - 1) * reviewsperPage, page * reviewsperPage)
         .map(review => (
           <ProductReview
             key={review.id}
@@ -56,6 +79,17 @@ const ProductReviews = ({ product, edit, setEdit }) => {
             review={review}
           />
         ))}
+      <Grid item container justifyContent="flex-end">
+        <Grid item>
+          <Pagination
+            sx={sx.pagination}
+            count={numPages}
+            page={page}
+            onChange={(e, newPage) => setPage(newPage)}
+            color="primary"
+          />
+        </Grid>
+      </Grid>
     </Grid>
   )
 }
