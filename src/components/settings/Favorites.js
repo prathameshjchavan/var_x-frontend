936053ee473f0feb2c7ff9e6b5fd4fa67132fed4
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-02-06 09:41:04 +0530

    favorited product details

--- src/components/settings/Favorites.js
+++ src/components/settings/Favorites.js
@@ -1,10 +1,14 @@
-import React, { useContext } from "react"
-import { UserContext } from "../../contexts"
+import React, { useContext, useState, useEffect } from "react"
+import { UserContext, FeedbackContext } from "../../contexts"
+import { setSnackbar } from "../../contexts/actions"
 import { DataGrid } from "@mui/x-data-grid"
 import { Grid } from "@mui/material"
+import axios from "axios"
 
 const Favorites = () => {
   const { user } = useContext(UserContext)
+  const [products, setProducts] = useState([])
+  const { dispatchFeedback } = useContext(FeedbackContext)
   const columns = [
     { field: "item", headerName: "Item", width: 250 },
     { field: "variant", headerName: "Variant", width: 275, sortable: false },
@@ -20,6 +24,30 @@ const Favorites = () => {
       width: "100%",
     },
   }
+
+  // useEffects
+  useEffect(() => {
+    axios
+      .get(`${process.env.STRAPI_API_URL}/api/favorites/userFavorites`, {
+        headers: { Authorization: `Bearer ${user.jwt}` },
+      })
+      .then(response => {
+        setProducts(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+
+        dispatchFeedback(
+          setSnackbar({
+            status: "error",
+            message:
+              "There was a problem getting your favorite products. Please try again.",
+          })
+        )
+      })
+  }, [])
+
+  console.log(products)
 
   return (
     <Grid item container sx={sx.container}>
