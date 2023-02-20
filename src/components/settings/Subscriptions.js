c7020a7cd4ab21a4fcf797c5c82629e3b7b72d1a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-02-20 10:33:51 +0530

    subscriptions columns

--- src/components/settings/Subscriptions.js
+++ src/components/settings/Subscriptions.js
@@ -3,14 +3,31 @@ import { Grid } from "@mui/material"
 import axios from "axios"
 import { UserContext, FeedbackContext } from "../../contexts"
 import { setSnackbar } from "../../contexts/actions"
+import SettingsGrid from "./SettingsGrid"
 
-const Subscriptions = () => {
+const Subscriptions = ({ setSelectedSetting }) => {
   const { user } = useContext(UserContext)
   const { dispatchFeedback } = useContext(FeedbackContext)
   const [subscriptions, setSubscriptions] = useState([])
 
   // sx prop
   const sx = {}
+
+  // data
+  const columns = [
+    { field: "details", headerName: "Details", width: 250, sortable: false },
+    { field: "item", headerName: "Item", width: 250, sortable: false },
+    { field: "quantity", headerName: "Quantity", width: 250, sortable: false },
+    {
+      field: "frequency",
+      headerName: "Frequency",
+      width: 250,
+      sortable: false,
+    },
+    { field: "next order", headerName: "Next Order", width: 250 },
+    { field: "total", headerName: "Total", width: 250 },
+    { field: "", width: 250, sortable: false },
+  ]
 
   // useEffects
   useEffect(() => {
@@ -33,9 +50,14 @@ const Subscriptions = () => {
       })
   }, [])
 
-  console.log(subscriptions)
-
-  return <Grid container>Subscriptions</Grid>
+  return (
+    <SettingsGrid
+      setSelectedSetting={setSelectedSetting}
+      rows={[]}
+      columns={columns}
+      rowsPerPage={3}
+    />
+  )
 }
 
 export default Subscriptions
